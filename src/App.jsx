bb86d19dd49e94b462e7b4d47e81a608a6764492
foxystore143 <mdkaifkhan093@gmail.com>
2024-03-03 09:35:28 +0530

    admin login create and dashborad

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { SignIn } from "./components/SignIn";
 import { SignUp } from "./components/SignUp";
 import { Home } from "./components/Home";
+import { Admin } from "./components/Admin";
+import { AdminDashboard } from "./components/AdminDashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
         <Route path="/home" element={<Home />}></Route>
         <Route path="/register" element={<SignUp />}></Route>
         <Route path="/login" element={<SignIn />}></Route>
+        <Route path="/Admin" element={<Admin />}></Route>
+        <Route path="/AdminDashboard" element={<AdminDashboard/>}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/AdminDashboard.jsx
@@ -0,0 +1,10 @@
+export const AdminDashboard = () => {
+    return (
+    <div className="container mx-auto mt-8">
+      <h1 className="text-4xl font-bold mb-4">Library Management System Admin Dashboard</h1>
+      {/* Add your dashboard components and functionalities here */}
+    </div>
+  );
+}
+
+export default AdminDashboard;
